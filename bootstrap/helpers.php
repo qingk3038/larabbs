041f6e79682e27b3c573519685497c28cca5b639
ourh <512574561@qq.com>
2018-01-24 20:59:44 +0800

    Finish basic page layout

--- bootstrap/helpers.php
+++ bootstrap/helpers.php
@@ -1,1 +1,7 @@
 <?php
+
+// 将路由名称转换为 CSS 类名
+function route_class()
+{
+    return str_replace('.', '-', Route::currentRouteName());
+}
---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,11 @@
+<footer class="footer">
+    <div class="container">
+        <p class="pull-left">
+            由
+            <a href="javascript:void(0);" target="_blank">ORH</a>
+            设计和编码
+            <span>❤</span>
+        </p>
+        <p class="pull-right"><a href="mailto:name@email.com">联系我们</a></p>
+    </div>
+</footer>
