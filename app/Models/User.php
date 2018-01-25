f62423f3c7f61c1af50fdec4ef6139d8581ce20a
ourh <512574561@qq.com>
2018-01-25 23:03:16 +0800

    List topics created by user

--- app/Models/User.php
+++ app/Models/User.php
@@ -31,4 +31,9 @@ class User extends Authenticatable
         'password',
         'remember_token',
     ];
+
+    public function topics()
+    {
+        return $this->hasMany(Topic::class);
+    }
 }
