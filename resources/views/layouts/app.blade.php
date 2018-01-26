5e11768d9684bedee5453b1595f9b6c3d00ba32b
ourh <512574561@qq.com>
2018-01-26 21:19:54 +0800

    Topic detail page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="description" content="@yield('description', 'LaraBBS 爱好者社区')" />
     <title>@yield('title', config('app.name')) - Laravel 进阶教程</title>
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     @yield('styles')
