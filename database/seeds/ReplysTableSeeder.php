e09b6df3a0743f7c7f94d6344e2478410e440c8d
ourh <512574561@qq.com>
2018-01-27 21:39:32 +0800

    List user replies

---
+++ database/seeds/ReplysTableSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Models\Reply;
+use App\Models\User;
+use App\Models\Topic;
+
+class ReplysTableSeeder extends Seeder
+{
+    public function run()
+    {
+        // 获取 Faker 实例
+        $faker = app(Faker\Generator::class);
+
+        // 所有用户 ID 数组，如：[1,2,3,4]
+        $userIds = User::all()->pluck('id')->toArray();
+
+        // 所有话题 ID 数组，如：[1,2,3,4]
+        $topicIds = Topic::all()->pluck('id')->toArray();
+
+        $replys = factory(Reply::class)
+            ->times(50)
+            ->make()
+            ->each(function ($reply, $index) use ($faker, $userIds, $topicIds) {
+                // 从用户 ID 数组中随机取出一个并赋值
+                $reply->user_id = $faker->randomElement($userIds);
+
+                // 话题 ID，同上
+                $reply->topic_id = $faker->randomElement($topicIds);
+            });
+
+        // 将数据集合转换为数组，并插入到数据库中
+        Reply::insert($replys->toArray());
+    }
+
+}
