72393c5e9e845c6726af6240d0d239984090ab47
ourh <512574561@qq.com>
2018-01-29 21:05:33 +0800

    Sidebar links

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -3,18 +3,21 @@
 namespace App\Http\Controllers;
 
 use App\Models\Category;
+use App\Models\Link;
 use App\Models\Topic;
 use App\Models\User;
 use Illuminate\Http\Request;
 
 class CategoriesController extends Controller
 {
-    public function show(Request $request, Category $category, User $user)
+    public function show(Request $request, Category $category, User $user, Link $link)
     {
         $topics = Topic::withOrder($request->order)->where('category_id', $category->id)->paginate();
 
         $active_users = $user->getActiveUsers();
 
-        return view('topics.index', compact('topics', 'category', 'active_users'));
+        $links = $link->getAllCached();
+
+        return view('topics.index', compact('topics', 'category', 'active_users', 'links'));
     }
 }
--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Handlers\ImageUploadHandler;
 use App\Models\Category;
+use App\Models\Link;
 use App\Models\Topic;
 use App\Models\User;
 use Illuminate\Http\Request;
@@ -18,11 +19,13 @@ class TopicsController extends Controller
         $this->middleware('auth', ['except' => ['index', 'show']]);
     }
 
-	public function index(Request $request, User $user)
+	public function index(Request $request, User $user, Link $link)
 	{
 	    $active_users = $user->getActiveUsers();
 		$topics = Topic::withOrder($request->order)->paginate();
-		return view('topics.index', compact('topics', 'active_users'));
+		$links = $link->getAllCached();
+
+		return view('topics.index', compact('topics', 'active_users', 'links'));
 	}
 
     public function show(Request $request, Topic $topic)
